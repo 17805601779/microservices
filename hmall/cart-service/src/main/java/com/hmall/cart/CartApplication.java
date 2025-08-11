934b6c8267f086b33411cfa77aa3131eca4dc270
hsy <9010480+hhhhyyyyy@user.noreply.gitee.com>
2025-08-11 20:03:52 +0800

    使用RestTemplate远程调用，nacos注册中心监控服务，实现购物车和商品服务从单体架构拆分

---
+++ hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -0,0 +1,14 @@
+package com.hmall.cart;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@MapperScan("com.hmall.cart.mapper")
+@SpringBootApplication
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+}
