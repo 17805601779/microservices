7c4e169175878a12207653397cadce9657d7456f
hsy <9010480+hhhhyyyyy@user.noreply.gitee.com>
2025-08-12 19:18:14 +0800

    update:搭建Seata组件,实现分布式事务一致性:XA模式/AT模式

--- hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("com.hmall.cart.mapper")
-@SpringBootApplication
+@SpringBootApplication()//scanBasePackages = {"com.hmall.api.client"}
 @EnableFeignClients(basePackages = "com.hmall.api.client",defaultConfiguration = DefaultFeignConfig.class)
 public class CartApplication {
     public static void main(String[] args) {
